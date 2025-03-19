8d77da269836264d06668537bd6bf15c4f1306e4
Ankitha-Karyam <ankithakaryam123@gmail.com>
2025-03-19 21:57:51 +0530

    del

--- src/main/java/com/cts/FuelDrop/Controller/UserController.java
+++ src/main/java/com/cts/FuelDrop/Controller/UserController.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +33,6 @@ public class UserController {
 		String userId = jwt.getClaim("userid").asString();
 		return userId;
 	}
-
 //	@GetMapping("/user/{userid}")
 //	@PreAuthorize("#userid== authentication.principal.userid")
 //	public ResponseEntity<?> getUserById(@PathVariable int userid) {
